f37f890f0d83c58c92e8e9b83da834ac31aa1d58
Daniel Kommers Contessa <danielkcontessa@gmail.com>
2025-12-13 19:40:00 -0300

    feat: iniciei o código js

--- codigo.js
+++ codigo.js
@@ -1,3 +1,7 @@
-aaa
+let saldo_bancario = document.getElementById("saldo_bancario").value;
+let saldo_inicial = document.getElementById('saldo_inicial');
+saldo_inicial.textContent = 'saldo_bancario';
 
-saldo_bancario = document.getElement
+saldo_bancario = parseFloat(saldo_bancario.replace(".", "").replace(",", "."));
+
+
