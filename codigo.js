840f52110dfae02e567de40467f72f7cd58fe115
Daniel Kommers Contessa <danielkcontessa@gmail.com>
2025-12-13 23:14:23 -0300

    feat: terminei o código para calcular e adicionar os valores na tabela

--- codigo.js
+++ codigo.js
@@ -1,7 +1,21 @@
-let saldo_bancario = document.getElementById("saldo_bancario").value;
-let saldo_inicial = document.getElementById('saldo_inicial');
-saldo_inicial.textContent = 'saldo_bancario';
+let saldo_bancario = 0;
+let valor_despesa = 0;
 
-saldo_bancario = parseFloat(saldo_bancario.replace(".", "").replace(",", "."));
+document.getElementById('saldo').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        saldo_bancario = parseFloat(this.value.replace(".", "").replace(",", "."));
+        document.getElementById('saldo_inicial').textContent = `R$ ${saldo_bancario.toFixed(2).replace(".", ",")}`;
+            
+}});
+
+document.getElementById('despesa').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        valor_despesa = parseFloat(this.value.replace(".", "").replace(",", "."));
+        saldo_bancario -= valor_despesa;
+        document.getElementById('tabela').innerHTML += `<tr><td>R$ ${valor_despesa.toFixed(2).replace(".", ",")}</td><td>R$ ${saldo_bancario.toFixed(2).replace(".", ',')}</td></tr>`;
+        document.getElementById('despesa').value = '';     
+    }});
+
+
 
 
